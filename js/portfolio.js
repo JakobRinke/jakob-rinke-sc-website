120a3db2262b49c07902083bcbc945d850727238
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2024-08-24 11:19:24 +0200

    done.

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,5 +13,9 @@ document.getElementById('project-description').innerText = project.description
     .replace(/\n/g, "")
     .replace(/\t/g, '')
 
+if (project.link) {
+    document.getElementById('project-link').href = project.link;
+    document.getElementById('project-link').classList.remove('invisible');
+}
 
 document.getElementById('project-image').src = "assets/img/portfolio/thumbnails/" + projectName+ ".jpg";
--- js/projects.js
+++ js/projects.js
@@ -1,29 +1,58 @@
 const projects = {
 
 
+    "idealo_crawler": {
+        name: "Idealo Crawler",
+        description: `Ein Webcrawler, der die Preise von Produkten auf Idealo.de sammelt und versucht gute Arbitrage-Möglichkeiten für den
+                        Amazon FBA-Handel zu finden. Hierzu verwendet er verschiedene APIs und Web-Scraping-Techniken, um die Daten zu sammeln 
+                        und speichert die Ergebnisse einer Google Tabelle. Der Crawler ist in Python geschrieben und ist Open-Source verfügbar.`,
+        link: "https://github.com/JakobRinke/idealo_crawler"
+    },
+
+    "fiesta_finder": {
+        name: "Fiesta Finder",
+        description: `Fiesta Finder ist eine App, die ich mit zwei Freunden entwickelt habe.
+                        Die App macht es einfach, Partys und Feiern in der Nähe zu finden und öffentlich zu teilen.
+                        Die App ist aktuell als Web Version verfügbar und wird in Zukunft
+                        auch als App für Android verfügbar sein. 
+                        `,
+        link: "https://fiesta-finder-d9abd.web.app/"
+    },
+
+    "fahrtenbuch": {
+        name: "Fahrtenbuch",
+        description: `Ich habe eine Fahrtenbuchapp entwickelt, welche die Verwaltung von Fahrten und die Berechnung von
+                        Entfernungen und Kosten erleichtert. Die App ist aktuell als Web Version verfügbar und wird in Zukunft
+                        auch als App für Android verfügbar sein. `,
+        link: "https://fahrtenbuch-app-2.web.app"
+    },
+
     "grouping": {
-        "name": "Grouping",
-        "description": `Ich habe mit meinen Freunden an einem Projekt gearbeitet, 
+        name: "Grouping",
+        description: `Ich habe mit meinen Freunden an einem Projekt gearbeitet, 
                         das mit Flutter entwickelt wurde. Unser Projekt, genannt Grouping, wurde entwickelt, 
                         um die Herausforderungen des  Eventmanagements zu bewältigen und die Funktionalität von
                         Gruppen im Vergleich zu anderen Social-Media-Apps wie WhatsApp zu verbessern. Aufgrund der 
                         erheblichen Kosten für die Wartung einer App und die Listung in App-Stores haben wir 
                         uns entschieden, sie ausschließlich als Open-Source-Docker-Version verfügbar zu machen, 
                         die Benutzer selbst hosten können. 
-                        Die App ist für iOS, Android und Windows verfügbar.`,         
+                        Die App ist für iOS, Android und Windows verfügbar.`,
+        link: "https://github.com/CodingBubble/FrontEnd"
     },
 
 
     "tag_offen": {
-        "name": "Tag der Offenen Tür am St. Josef Gymnasium",
-        "description": `Angesichts der Herausforderungen durch die Covid-19-Pandemie benötigte unsere ehemalige Schule
+        name: "Tag der Offenen Tür am St. Josef Gymnasium",
+        description: `Angesichts der Herausforderungen durch die Covid-19-Pandemie benötigte unsere ehemalige Schule
          eine innovative Lösung, um ihren jährlichen Tag der offenen Tür online zu veranstalten. In Zusammenarbeit mit einer
           engagierten Gruppe von Freunden haben wir ein Projekt gestartet, um eine Website zu erstellen, die einen virtuellen Rundgang
            durch die Schule bietet und dabei jedes Fachgebiet und verschiedene andere Aspekte der Schulumgebung zeigt. Diese erste
             Version wurde 2021 realisiert, und ein Jahr später wurde die Website durch eine Verbesserung des Gesamtdesigns neu gestaltet. 
             Bis 2023 haben wir diese virtuelle Tour erfolgreich in die offizielle Website der Schule integriert. Mein Hauptaugenmerk lag 
             dabei auf der Backend-Entwicklung und der Technik, um sicherzustellen, dass das Online-Erlebnis richtig und schnell geladen wird.
-             der Schule effektiv repräsentiert. `
+             der Schule effektiv repräsentiert. `,
+
+        link: "https://schulwebsite-test.rich-smartroom.de/schulrundgang.html",
                 
     },
 
@@ -44,7 +73,8 @@ const projects = {
          Dieses System ermöglicht es Lehrern, mühelos Nachrichtenbeiträge zu erstellen, die Einblicke in das 
          tägliche Leben an unserer Schule geben. Während dieses Projekts haben wir wertvolle Erfahrungen in der 
          Zusammenarbeit mit Kunden gesammelt, das Projekt an ihre spezifischen Anforderungen angepasst und durch 
-         mehrere Feedback-Umfragen die Funktionalität der Website optimiert. `
+         mehrere Feedback-Umfragen die Funktionalität der Website optimiert. `,
+        link: "https://schulwebsite-test.rich-smartroom.de/"
     },
 
     "live_game_lib": {
@@ -52,7 +82,9 @@ const projects = {
         description: `Dieses Projekt ist im Zuge der Software Challenge bei Inverso 2024 entstanden. 
         Wir haben ein Flutter-Framework entwickelt, das die Entwicklung von Echtzeit-Multiplayer-Spielen
          vereinfacht. Diese Bibliothek erleichtert den Prozess der Erstellung von fesselnden und interaktiven Multiplayer-Erlebnissen. 
-         Bei der Software Challenge erhielten wir den zweiten Platz und ein Preisgeld von 500€.`
+         Bei der Software Challenge erhielten wir den zweiten Platz und ein Preisgeld von 500€.`,
+
+        link: "https://pub.dev/packages/live_game_lib"
          
     },
 
@@ -60,8 +92,8 @@ const projects = {
     "waldguru": {
         name: "Webseite für die Waldguru Stiftung",
         description: `Ein bezahltes Projekt für die Waldguru Stiftung, bei dem ich eine Website erstellt habe.
-            Die Website zeigt die Ansicht eines neuen Lichtzeitkalenders, der von der Stiftung entwickelt wurde.    
-        `
+            Die Website zeigt die Ansicht eines neuen Lichtzeitkalenders, der von der Stiftung entwickelt wurde. `, 
+        link: "https://www.lemuria-waldguru.at/"
     }
 
 
